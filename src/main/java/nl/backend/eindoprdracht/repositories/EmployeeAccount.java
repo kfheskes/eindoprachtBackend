6766e88dce6765f629c06bad6fc800caf4b3d041
kfheskes <kayarneheskes@hotmail.com>
2023-12-17 13:41:56 +0100

    add repositories

---
+++ src/main/java/nl/backend/eindoprdracht/repositories/EmployeeAccount.java
@@ -0,0 +1,7 @@
+package nl.backend.eindoprdracht.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeAccount extends JpaRepository<EmployeeAccount, Long> {
+
+}
---
+++ src/main/java/nl/backend/eindoprdracht/repositories/WorkSchedule.java
@@ -0,0 +1,6 @@
+package nl.backend.eindoprdracht.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkSchedule extends JpaRepository <WorkSchedule, Long> {
+}
