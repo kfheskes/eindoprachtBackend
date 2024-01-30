a40352c377d11ee99d54502b443399abc100af0f
kfheskes <kayarneheskes@hotmail.com>
2024-01-30 19:51:04 +0100

    put more test data

---
+++ src/test/java/nl/backend/eindoprdracht/services/OrderServiceTest.java
@@ -0,0 +1,12 @@
+package nl.backend.eindoprdracht.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void getAllOrders() {
+    }
+}
